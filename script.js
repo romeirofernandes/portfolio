8469d3bdc5120063273ddc5adcfcf309649b94b4
Romeiro <theromeirofernandes@gmail.com>
2025-01-31 23:38:40 +0530

    complete project

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector(".fixed-header")
+  
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        header.classList.add("scrolled")
+      } else {
+        header.classList.remove("scrolled")
+      }
+    })
+  })
+
